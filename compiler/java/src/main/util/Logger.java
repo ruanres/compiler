231f6d455079afed55e52105b0e5e0ed5b663e23
jvmafra <joao.mafra@ccc.ufcg.edu.br>
2018-02-27 00:19:38 -0300

    Adicionando logs no parser

--- compiler/java/src/main/util/Logger.java
+++ compiler/java/src/main/util/Logger.java
@@ -5,7 +5,7 @@ public class Logger {
 
 	private static boolean activate_log = false;
 	
-	public static void printLog(String value) {
+	public static void print(String value) {
 		if (activate_log) {
 			System.out.println(value);
 		};
